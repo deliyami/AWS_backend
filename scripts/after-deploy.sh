bde80bb56b48d95030273f97bae696aff8e2601b
Hyeonseok Jang <jang.hyeonseok@linkbaljang.local>
2023-09-28 13:16:31 +0900

    FIX after deploy

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -7,4 +7,5 @@ sudo /usr/bin/pm2 kill
 sudo yarn
 # sudo /usr/bin/yarn db:pull # when use prisma
 # sudo /usr/bin/yarn generate # when use prisma
-sudo /usr/bin/pm2 start dist
+sudo mv .env dist/src/
+sudo /usr/bin/pm2 start dist/src/main.js
