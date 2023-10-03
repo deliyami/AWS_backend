8800495e6bb0cf426eaa3f3383cfbdadd7853670
deliyami <dkdidudtn@gmail.com>
2023-10-03 22:30:57 +0900

    ADD migration

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -7,6 +7,7 @@ sudo /usr/bin/pm2 kill
 npm i
 # sudo /usr/bin/yarn db:pull # when use prisma
 # sudo /usr/bin/yarn generate # when use prisma
+npm run typeorm migration:run -- -d dist/ormconfig.js
 sudo mv .env dist/src/
 cd dist/src
 sudo /usr/bin/pm2 start main.js
